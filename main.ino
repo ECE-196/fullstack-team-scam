4099b9e7094cbe86494970709d3ef3bdc25644dc
sbulatao <114209345+sbulatao@users.noreply.github.com>
2024-10-31 17:34:16 -0700

    updates

--- main.ino
+++ main.ino
@@ -5,33 +5,33 @@ const char S_OK { 0xaa };
 const char S_ERR { 0xff };
 
 
-void setup() {
-    pinMode(LED, OUTPUT);
-
-    // register "on_receive" as callback for RX event
-    USBSerial.onEvent(ARDUINO_HW_CDC_RX_EVENT, on_receive);
-    USBSerial.begin(9600);
-}
-
-void loop() {
-    USBSerial.println("Hello, World!");
-    delay(1000);
-}
-
 
 void on_receive(void* event_handler_arg, esp_event_base_t event_base, int32_t event_id, void* event_data) {
     // read one byte
-    char state { USBSerial.read() };
-
+    char state { Serial.read() };
     // guard byte is valid LED state
     if (!(state == LOW || state == HIGH)) {
         // invalid byte received
         // report error
-        USBSerial.write(S_ERR);
+        Serial.write(S_ERR);
         return;
     }
-
     // update LED with valid state
     digitalWrite(LED, state);
-    USBSerial.write(S_OK);
+    Serial.write(S_OK);
+}
+
+
+
+void setup() {
+    pinMode(LED, OUTPUT);
+
+    // register "on_receive" as callback for RX event
+    Serial.onEvent(ARDUINO_HW_CDC_RX_EVENT, on_receive);
+    Serial.begin(9600);
+}
+
+void loop() {
+    // Serial.println("Hello, World!");
+    // delay(1000);
 }
--- main.py
+++ main.py
@@ -1,50 +1,95 @@
-from serial import Serial, SerialException
-
-# with Serial('/dev/cu.usbmodem84AC34F50B811', 9600) as ser:
-#     while True:
-#         print(ser.readline().decode())
-
-####################
-
-# with Serial('/dev/cu.usbmodem84AC34F50B811', 9600) as ser:
-#     ser.write(bytes([0x1]))
-#     input() # keep port open to see the LED turn on
-
-####################
-
-# with Serial('/dev/cu.usbmodem84AC34F50B811', 9600) as ser:
-#     ser.write(bytes([0x1]))
-#     assert ser.read() == bytes([0xaa])# ASSERTION ERROR HERE
-
-#     ser.write(bytes([0x0]))
-#     assert ser.read() == bytes([0xaa])# ASSERTION ERROR HERE
-
-#     ser.write(bytes([0x2]))
-#     assert ser.read() == bytes([0xff])# ASSERTION ERROR HERE
-
-
-
 import tkinter as tk
 import tkinter.ttk as ttk
+from tkinter.messagebox import showerror
+from serialButNOT import Serial, SerialException
+from serial.tools.list_ports import comports
+from threading import Thread, Lock # we'll use Lock later ;)
+
+
+
+S_OK: int = 0xaa
+S_ERR: int = 0xff
+
+def detached_callback(f):
+    return lambda *args, **kwargs: Thread(target=f, args=args, kwargs=kwargs).start()
+
+
+class LockedSerial(Serial):
+    _lock: Lock = Lock()
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def read(self, size=1) -> bytes:
+        with self._lock:
+            return super().read(size)
+        
+    def write(self, b: bytes, /) -> int | None:
+        with self._lock:
+            super().write(b)
+
+    def close(self):
+        with self._lock:
+            super().close()
+            
+
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
+
         self.title("LED Blinker")
+        self.led = tk.BooleanVar()
+        self.port = tk.StringVar() # add this
 
+        ttk.Checkbutton(self, text='Toggle LED', variable=self.led, command=self.update_led).pack()
+        ttk.Button(self, text='Send Invalid', command=self.send_invalid).pack()
+        ttk.Button(self, text='Disconnect',command=self.disconnect, default='active').pack()
+        SerialPortal(self) # and this
+    
+    def connect(self):
+        self.ser = LockedSerial(self.port.get())
+
+    def disconnect(self):
+        self.ser.close()
+        SerialPortal(self) # display portal to reconnect
+
+    def write(self, b: bytes):
+        try:
+            self.ser.write(b)
+            if int.from_bytes(self.ser.read(), 'big') == S_ERR:
+                showerror('Device Error', 'The device reported an invalid command.')
+        except SerialException:
+            showerror('Serial Error', 'Write failed.')
+
+    @detached_callback
+    def update_led(self):
+        self.write(bytes([self.led.get()]))
+
+    def send_invalid(self):
+        self.write(bytes([0x10]))
+
+    def __enter__(self):
+        return self
+    def __exit__(self, *_):
+        self.disconnect()
+
+
+class SerialPortal(tk.Toplevel):
+    def __init__(self, parent: App):
+        super().__init__(parent)
+        self.parent = parent
+        self.parent.withdraw() # hide App until connected
+        ttk.OptionMenu(self, parent.port, '', *[d.device for d in comports()]).pack()
+        ttk.Button(self, text='Connect', command=self.connect, default='active').pack()
+
+    def connect(self):
+        self.parent.connect()
+        self.destroy()
+        self.parent.deiconify() # reveal App
+
+
+
+    
 if __name__ == '__main__':
-    app = App()
-    app.mainloop()
-
-
-def __init__(self):
-    super().__init__()
-    self.title("LED Blinker")
-    ttk.Checkbutton(self, text='Toggle LED').pack()
-    ttk.Button(self, text='Send Invalid').pack()
-    ttk.Button(self, text='Disconnect', default='active').pack()
-
-
-
-
-
-
+    with App() as app:
+        app.mainloop()
+        
